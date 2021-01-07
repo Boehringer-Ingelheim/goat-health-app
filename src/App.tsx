cc526fafd4a54bb2c88694afc6f3546c30f18de2
Simon G <development@gol.ms>
2021-01-07 20:15:08 +0100

    style: order imports alphabetically

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { Menu } from './components/Menu';
-import { HomePage } from './pages/Page';
-import { ChapterPage } from './pages/Chapter';
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 /* Basic CSS for apps built with Ionic */
@@ -23,12 +21,15 @@ import '@ionic/react/css/display.css';
 // import './theme/variables.css';
 import './theme/bright.theme.css';
 import './theme/dark.theme.css';
-// import Tutorial from './pages/Tutorial';
-import { SettingsPage } from './pages/Settings';
-import { AboutPage } from './pages/About';
-import { SearchPage } from './pages/Search';
-import { THEMES } from './utils/theme';
+import { Menu } from './components/Menu';
 import { selectCurrentTheme } from './data/user/user.selector';
+import { AboutPage } from './pages/About';
+import { ChapterPage } from './pages/Chapter';
+import { HomePage } from './pages/Page';
+import { SearchPage } from './pages/Search';
+import { SettingsPage } from './pages/Settings';
+// import Tutorial from './pages/Tutorial';
+import { THEMES } from './utils/theme';
 
 const App: React.FC = () => {
   const currentTheme = useSelector(selectCurrentTheme);
