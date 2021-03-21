c71e49151c931d77078413683453aa100a3a8a52
Simon G <development@gol.ms>
2021-03-21 10:24:14 +0100

    test: add app screenshots

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+const BASE_URL = 'http://localhost:8080';
 
-test('renders without crashing', () => {
-  const { baseElement } = render(<App />);
-  expect(baseElement).toBeDefined();
+describe('App Screenshots', () => {
+  it('/home', async () => {
+    await page.goto(`${BASE_URL}/home`);
+    await page.screenshot({
+      path: `dist/${deviceName}-screenshot-home.png`
+        .toLowerCase()
+        .replace(' ', '-'),
+    });
+  });
+
+  const CHAPTERS = ['01', '02'];
+  for (let i = 0; i < CHAPTERS.length; i++) {
+    it(`/chapter/${CHAPTERS[i]}/01`, async () => {
+      await page.goto(`${BASE_URL}/chapter/${CHAPTERS[i]}/01`);
+      await page.screenshot({
+        path: `dist/${deviceName}-screenshot-chapter-${CHAPTERS[i]}-01.png`
+          .toLowerCase()
+          .replace(' ', '-'),
+      });
+    });
+  }
 });
+
+export {};
