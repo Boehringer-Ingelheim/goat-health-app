950c76a232581abbcc4ae98df0163a33d2b31bf4
Simon G <development@gol.ms>
2021-03-29 09:45:32 +0200

    test(screenshots): update test cases

---
+++ scripts/screenshots/screenshots.stores.test.tsx
@@ -0,0 +1,87 @@
+import { getBaseUrl, getPathScreenshot } from './utils';
+
+const {
+  JEST_PLAYWRIGHT_CONTEXT_OPTION_LOCAL,
+  JEST_PLAYWRIGHT_SCREENSHOT_SEARCH_QUERY,
+} = process.env;
+
+describe('App Screenshots', () => {
+  it('home', async () => {
+    await page.goto(`${getBaseUrl()}/home`, { waitUntil: 'networkidle' });
+    await page.screenshot({
+      path: getPathScreenshot(
+        JEST_PLAYWRIGHT_CONTEXT_OPTION_LOCAL,
+        deviceName,
+        'home',
+      ),
+    });
+  });
+
+  it('chapters', async () => {
+    await page.goto(`${getBaseUrl()}/home`, { waitUntil: 'networkidle' });
+    await page.click(
+      '//html/body/div/ion-app/ion-split-pane/ion-router-outlet/div/ion-header/ion-toolbar/ion-buttons/ion-menu-button',
+    );
+    await page.waitForSelector('#main.menu-content-open');
+    await page.screenshot({
+      path: getPathScreenshot(
+        JEST_PLAYWRIGHT_CONTEXT_OPTION_LOCAL,
+        deviceName,
+        'chapters',
+      ),
+    });
+  });
+
+  it(`illustrations`, async () => {
+    await page.goto(`${getBaseUrl()}/chapter/02/01`, {
+      waitUntil: 'networkidle',
+    });
+    await page.screenshot({
+      path: getPathScreenshot(
+        JEST_PLAYWRIGHT_CONTEXT_OPTION_LOCAL,
+        deviceName,
+        `illustrations`,
+      ),
+    });
+  });
+
+  it('search', async () => {
+    await page.goto(
+      `${getBaseUrl()}/search?q=${JEST_PLAYWRIGHT_SCREENSHOT_SEARCH_QUERY}`,
+      { waitUntil: 'networkidle' },
+    );
+    await page.waitForSelector('ion-card.hydrated');
+    await page.screenshot({
+      path: getPathScreenshot(
+        JEST_PLAYWRIGHT_CONTEXT_OPTION_LOCAL,
+        deviceName,
+        'search',
+      ),
+    });
+  });
+
+  it('accessibility', async () => {
+    await page.emulateMedia({ colorScheme: 'dark' });
+    await page.goto(`${getBaseUrl()}/settings`, { waitUntil: 'networkidle' });
+    await page.screenshot({
+      path: getPathScreenshot(
+        JEST_PLAYWRIGHT_CONTEXT_OPTION_LOCAL,
+        deviceName,
+        'accessibility',
+      ),
+    });
+  });
+
+  it('about', async () => {
+    await page.goto(`${getBaseUrl()}/about`, { waitUntil: 'networkidle' });
+    await page.screenshot({
+      path: getPathScreenshot(
+        JEST_PLAYWRIGHT_CONTEXT_OPTION_LOCAL,
+        deviceName,
+        'about-mmh',
+      ),
+    });
+  });
+});
+
+export {};
