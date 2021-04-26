c45e35938ff62bc297af0448369ddf601f94a94b
Simon G <development@gol.ms>
2021-04-26 10:33:44 +0200

    feat(chapters): add chapter content about earning money with goats

--- src/components/Chapters/ChapterContent/index.tsx
+++ src/components/Chapters/ChapterContent/index.tsx
@@ -1,7 +1,11 @@
 import { chapter01 } from './ChapterContent01';
 import { chapter02 } from './ChapterContent02';
+import { chapter03 } from './ChapterContent03';
+import { chapter04 } from './ChapterContent04';
 
 export const chapter = {
   '01': chapter01,
   '02': chapter02,
+  '03': chapter03,
+  '04': chapter04,
 };
