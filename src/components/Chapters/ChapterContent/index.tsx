bcc8a79b588fd2696eccb31161c25a38995b85c7
Simon G <development@gol.ms>
2021-04-27 10:01:21 +0200

    feat(chapters): add chapter content about how do diseases happen

--- src/components/Chapters/ChapterContent/index.tsx
+++ src/components/Chapters/ChapterContent/index.tsx
@@ -2,10 +2,14 @@ import { chapter01 } from './ChapterContent01';
 import { chapter02 } from './ChapterContent02';
 import { chapter03 } from './ChapterContent03';
 import { chapter04 } from './ChapterContent04';
+import { chapter05 } from './ChapterContent05';
+import { chapter06 } from './ChapterContent06';
 
 export const chapter = {
   '01': chapter01,
   '02': chapter02,
   '03': chapter03,
   '04': chapter04,
+  '05': chapter05,
+  '06': chapter06,
 };
