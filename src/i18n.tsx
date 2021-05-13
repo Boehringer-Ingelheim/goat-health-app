5483a24f835576880b6f278be0cbe08359fa71e3
Simon G <development@gol.ms>
2021-05-13 12:18:35 +0200

    feat(i18n): add tamil language support

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,22 +1,20 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import { enUS } from './translations';
+import { enUS, taIN } from './translations';
 
-export const I18N_DEFAULT = 'en-US';
+export const I18N_DEFAULT = 'en';
 
 // Language identifiers as specified by RFC 3066: https://www.i18nguy.com/unicode/language-identifiers.html
 export const I18N_LANGUAGES_SUPPORTED = [
   { cultureLang: 'en', name: 'English', nativeName: 'English' },
+  { cultureLang: 'ta', name: 'Tamil (India)', nativeName: 'தமிழ்' },
 ];
 
-// const translations = {
-//   'en-US': { translation: require('./translations/en-US.json') },
-//   'ta-IN': { translation: require('./translations/ta-IN.json') },
-// };
-
+// The recommendation is to use non-regional languages: https://github.com/i18next/i18next/issues/1564#issuecomment-788809464
 export const resources = {
   en: enUS,
+  ta: taIN,
 };
 
 export default i18n
@@ -28,12 +26,11 @@ export default i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    // resources: translations,
     resources,
     cleanCode: true,
     debug: false,
     ns: Object.keys(enUS),
-    supportedLngs: ['en'],
+    supportedLngs: ['en', 'ta'],
     fallbackLng: 'en',
     defaultNS: 'app',
     detection: {
