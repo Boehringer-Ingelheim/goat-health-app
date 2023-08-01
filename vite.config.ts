3f462417d2f188bfea859544f24a481862f9b363
Kaiser,Luca (HR Appr) BIG-DE-B <luca.kaiser@boehringer-ingelheim.com>
2023-08-01 14:02:35 +0200

    Saving a status with new react version

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "") };
+
+  return {
+    plugins: [react()],
+    server: {
+      open: Boolean(process.env.BROWSER),
+      port: Number(process.env.PORT) || 5173,
+    },
+    optimizeDeps: {
+      exclude: ['@ionic/react']
+    }
+  };
+});
